4b7d75cc86e315a1c78df9bca3b17aebf1eee978
YuriyRozhnov <ryispb@mail.ru>
2021-12-25 16:25:26 +0300

    python200_c25/12 по ....

---
+++ Основы ООП/Лабораторные задания/task2_Node_is_valid_set_node/task.py
@@ -0,0 +1,43 @@
+from typing import Any, Optional
+
+
+class Node:
+    """ Класс, который описывает узел связного списка. """
+
+    def __init__(self, value: Any, next_: Optional["Node"] = None):
+        """
+        Создаем новый узел для односвязного списка
+        :param value: Любое значение, которое помещено в узел
+        :param next_: следующий узел, если он есть
+        """
+        self.value = value
+
+        self.next = None
+        self.set_next(next_)
+
+        # TODO установить значение следующего узла с помощью метода set_next
+
+    def __repr__(self) -> str:
+        return f"Node({self.value}, {self.next})"
+
+    def is_valid(self, node: Any) -> None:
+        if not isinstance(node, (type(None), Node)):
+            raise TypeError
+        # TODO метод проверки корректности связываемого узла
+        ...
+    def set_next(self, next_: Optional["Node"] = None) -> None:
+        self.is_valid(next_)
+        self.next = next_
+        # TODO метод должен проверять корректность узла и устанавливать значение атрибуту next
+        ...
+
+if __name__ == '__main__':
+    first_node = Node(1)
+    second_node = Node(2)
+    # TODO инициализируйте два узла с любыми значениями
+
+    first_node.set_next(second_node)
+    # TODO свяжите первый узел со вторым
+
+    print(first_node)
+    print(second_node)
---
+++ Основы ООП/Практические задания/task1_Glass__init__/main.py
@@ -0,0 +1,43 @@
+from typing import Union
+
+
+class Glass:
+
+    def __init__(self, capacity_volume: Union[int, float], occupied_volume: Union[int, float]):
+
+        self.capacity_volume = self.__check_capacity_volume(capacity_volume)
+        self.occupied_volume = self.__check_occupied_volume(occupied_volume)
+
+        # self.__check_overflow(self.capacity_volume, self.occupied_volume)
+        self.__check_overflow()
+
+    @staticmethod
+    def __check_capacity_volume(value) -> Union[int, float]:
+        """
+        :param value:
+        :return:
+        """
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value <= 0:
+            raise ValueError
+        return value
+
+    @staticmethod
+    def __check_occupied_volume(value) -> Union[int, float]:
+        if not isinstance(value, (int, float)):
+            raise TypeError
+        if value < 0:
+            raise ValueError
+        return value
+
+    # @staticmethod
+    # def __check_overflow(capacity, occupied):
+    #     if capacity < occupied:
+    #         raise OverflowError('Стакан не резиновый')
+    def __check_overflow(self):
+        if self.capacity_volume < self.occupied_volume:
+            raise OverflowError('Стакан не резиновый')
+
+
+
